64cdcfbe620c8c49ae8f819efbe4c2cc2943e1bc
Harsh30lath <harsh408@gmail.com>
2026-03-31 17:55:42 +0530

    Cart Controller Changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const userRoutes = require('./Routes/userRoutes')
 const addressRoutes = require('./Routes/addressRoutes')
 const categoryRoutes =require('./Routes/categoryRoutes')
 const productRoutes = require('./Routes/productRoutes')
+const cartRoutes = require('./Routes/cartRoutes')
 const dotenv = require('dotenv').config();
 const connectDB = require('./Config/DBconnect')
 
@@ -15,6 +16,7 @@ app.use('/api/user',userRoutes)
 app.use('/api/user',addressRoutes)
 app.use('/api/category',categoryRoutes)
 app.use('/api/product',productRoutes)
+app.use('/api/cart',cartRoutes)
 
 app.listen(5000, () =>{
     console.log('Server is Serving at Port 5000')
