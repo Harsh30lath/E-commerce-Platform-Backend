3907573066348df405190789dbd67ee3170ea47c
Harsh30lath <harsh408@gmail.com>
2026-04-11 15:12:38 +0530

    PaymentControl Changes

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -1,5 +1,6 @@
 const asynchandler = require('express-async-handler')
 const instance = require('../Config/razorpay');
+const crypto = require("crypto");
 
 const processPayment = asynchandler(async(req,res) =>{
     const options = {
@@ -19,18 +20,25 @@ const sendAPIKey = asynchandler(async(req,res) =>{
     })
 })
 
-const webhook = asynchandler(async(req,res) =>{
-    const event = req.body.event;
+const paymentVerification = asynchandler(async(req,res) =>{
+    const {razorpay_payment_id,razorpay_order_id,razorpay_signature} = req.body;
+        
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto.createHmac('sha256',process.env.RAZORPAY_SECRET_KEY)
+    .update(body.toString())
+    .digest('hex');
+        
+    if (expectedSignature === razorpay_signature){
+        return res.status(200).json({
+        success: true,
+        message: "Payment Verified"
+    })
+    } else {
+        return res.status(400).json({
+        success: false,
+        message: "Invalid Signature"
+        })
+    }}
+);
 
-    if (event === "payment.captured") {
-        const payment = req.body.payload.payment.entity;
-
-        console.log(payment.id);
-        console.log(payment.order_id);
-
-    }
-
-    res.status(200).json({ status: "ok" });
-})
-
-module.exports = {processPayment,sendAPIKey,webhook}
+module.exports = {processPayment,sendAPIKey,paymentVerification}
--- Routes/paymentRoutes.js
+++ Routes/paymentRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const validationToken = require('../middleware/ValidateToken');
-const { processPayment,sendAPIKey,webhook} = require('../Controllers/paymentController')
+const { processPayment,sendAPIKey,paymentVerification} = require('../Controllers/paymentController')
 
 router.post('/process',validationToken,processPayment)
 router.get('/getkey',validationToken,sendAPIKey)
-router.post('/webhook',validationToken,webhook)
+router.post('/ge',validationToken,paymentVerification)
 
 module.exports = router;
