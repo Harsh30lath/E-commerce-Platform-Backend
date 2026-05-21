8a5641d47cd2145eb10644292f211506b6c83760
Harsh30lath <harsh408@gmail.com>
2026-05-21 19:49:01 +0530

    App.js Changes

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api/payment',paymentRoutes)
 
 
 app.get ('/', (req,res) =>{
-    res.status(200).send("Hiii backend is running");
+    res.status(200).send("Your Backend is running");
 })
 
 
